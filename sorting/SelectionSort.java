1c149596adc839128615fd893254409067d33991
Aayush Kumar Singha <aayushsingh.mail@gmail.com>
2018-10-04 04:10:27 +0530

    Create SelectionSort.java (#6)

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,17 @@
+public class SelectionSort{
+    public int[] selection_sort(final int array[]) {
+        final int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            final int temp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+}
